eca6c24d99ce002d3eace6413b7f50a8530b8da5
LAPTOP-482R93HS\ASUS <mauludyrakhman@gmail.com>
2023-10-16 13:53:01 +0700

    project ecommerce

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-"# online-shop-ecommerce" 
+Clone Projek
+
+```bash
+  git clone https://github.com/abdulaziz-m5u/toko-online-laravel.git nama_projek
+```
+
+Masuk ke folder dengan perintah
+
+```bash
+  cd nama_projek
+```
+
+-   Copy .env.example menjadi .env kemudia edit database dan api key nya
+
+```bash
+    composer install
+```
+
+```bash
+    npm i
+```
+
+```bash
+    php artisan key:generate
+```
+
+```bash
+    php artisan artisan migrate:fresh --seed
+```
+
+```bash
+    php artisan storage:link
+```
+
+```bash
+    npm run dev
+```
+
+```bash
+    php artisan serve
+```
+
+#### Login ADMIN
+
+-   email = admin@admin.com
+-   password = 123
---
+++ resources/views/frontend/layout.blade.php
@@ -0,0 +1,152 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Favicon -->
+        <!-- <link rel="shortcut icon" type="image/x-icon" href="{{ asset('themes/ezone/assets/img/favicon.png') }}"> -->
+		
+		<!-- all css here -->
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/magnific-popup.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/animate.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/owl.carousel.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/themify-icons.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/pe-icon-7-stroke.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/icofont.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/meanmenu.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/easyzoom.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/bundle.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('themes/ezone/assets/css/responsive.css') }}">
+        <script src="{{ asset('themes/ezone/assets/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+
+        <!-- CSRF Token -->
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <!-- header -->
+
+        <header>
+            <div class="header-top-furniture wrapper-padding-2 res-header-sm">
+                <div class="container-fluid">
+                    <div class="header-bottom-wrapper">
+                        <div class="logo-2 furniture-logo ptb-30">
+                            <a href="/">
+                                <!-- <img src="{{ asset('themes/ezone/assets/img/logo/2.png') }}" alt=""> -->
+                            </a>
+                        </div>
+                        <div class="menu-style-2 furniture-menu menu-hover">
+                            <nav>
+                                <ul>
+                                    <li><a href="/">home</a></li>
+                                    <li><a href="{{url('products')}}">Product</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="header-cart">
+                            <a class="icon-cart-furniture" href="{{ url('carts') }}">
+                                <i class="ti-shopping-cart"></i>
+                                <span class="shop-count-furniture green">{{ Cart::count() }}</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mobile-menu-area d-md-block col-md-12 col-lg-12 col-12 d-lg-none d-xl-none">
+                            <div class="mobile-menu">
+                                <nav id="mobile-menu-active">
+                                    <ul class="menu-overflow">
+                                        <li><a href="/">home</a></li>
+                                        <li><a href="{{url('products')}}">Product</a></li>
+                                    </ul>
+                                </nav>							
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-bottom-furniture wrapper-padding-2 border-top-3">
+                <div class="container-fluid">
+                    <div class="furniture-bottom-wrapper">
+                        <div class="furniture-login">
+                            <ul>
+                                @guest
+                                    <li>Get Access: <a href="{{ url('login') }}">Login</a></li>
+                                    <li><a href="{{ url('register') }}">Register</a></li>
+                                @else
+                                    <li>Hello: <a href="{{ url('profile') }}">{{ Auth::user()->first_name }}</a></li>
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                @endguest
+                            </ul>
+                        </div>
+                        <div class="furniture-wishlist">
+                            <ul>
+                                <li><a href="{{ url('wishlists') }}"><i class="ti-heart"></i> Favorites</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- end -->
+        
+        @yield('content')
+
+        <!-- footer -->
+        <footer class="footer-area">
+            <div class="footer-bottom ptb-20 gray-bg-8">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <div class="copyright-furniture">
+                                <p>Copyright © <a href="https://hastech.company/">HasTech</a> .Customize by Me 2023. All Right Reserved.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end -->
+        <div id="loader" style="display: none;">
+            <div id="loading" style="z-index:99999;position: fixed;top:0;left:0;right:0;bottom:0;background-color:rgba(0,0,0,.3);display: flex;justify-content:center;align-items: center;" class="mx-auto">
+                <p><img src="{{ asset('themes/ezone/assets/img/loading.gif') }}" /> Please Wait</p>
+            </div>
+        </div>
+		
+		<!-- all js here -->
+        <script src="{{ asset('themes/ezone/assets/js/vendor/jquery-1.12.0.min.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/popper.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/jquery.magnific-popup.min.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/isotope.pkgd.min.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/imagesloaded.pkgd.min.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/jquery.counterup.min.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/waypoints.min.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/ajax-mail.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/owl.carousel.min.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/plugins.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/main.js') }}"></script>
+        <script src="{{ asset('themes/ezone/assets/js/app.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script>
+            $(".delete").on("click", function () {
+                return confirm("Do you want to remove this?");
+            });
+        </script>
+        @stack('script-alt')
+    </body>
+</html>
